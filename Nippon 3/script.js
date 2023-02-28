b7e05c00767ecf6cb0488602c4f53253a73bfed5
maddy <112689046+maddyengblad@users.noreply.github.com>
2023-02-28 14:30:56 +0100

    Add files via upload
    v3 2/28/2023

---
+++ Nippon 3/script.js
@@ -0,0 +1,27 @@
+
+let mybutton = document.getElementById("myBtn")
+let nav = document.getElementById("navbar")
+let navimg = document.getElementById("logoBtn")
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) {
+    mybutton.style.display = "block";
+    navimg.style.display = "block";
+    nav.style.position = "fixed";
+    nav.style.top = "0";
+
+  } else {
+    mybutton.style.display = "none";
+    navimg.style.display = "none";
+    nav.style.position = "relative";
+
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
